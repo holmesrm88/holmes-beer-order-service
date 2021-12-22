9e1f6e86f587e47f6f2b339bd831518b1935d7ca
holmesrm88 <holmesrm88@gmail.com>
2021-12-22 15:19:34 -0500

    save upc

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -37,13 +37,14 @@ public class BeerOrderLine extends BaseEntity {
 
     @Builder
     public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
+                         BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity,
                          Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
+        this.upc = upc;
     }
 
     @ManyToOne
@@ -51,5 +52,6 @@ public class BeerOrderLine extends BaseEntity {
 
     private UUID beerId;
     private Integer orderQuantity = 0;
+    private String upc;
     private Integer quantityAllocated = 0;
 }
